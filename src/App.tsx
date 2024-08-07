dec9e3a82207dc8620ffb1235e90b6f35b3a45b9
hebirong <hebirong@igengmei.com>
2024-08-07 11:46:22 +0800

    feat(init):init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import unielonLogo from '/unielon.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://unielon.com/" target="_blank">
+          <img src={unielonLogo} className="logo" alt="Unielon logo" />
+        </a>
+      </div>
+      <h3>Unielon Telegram</h3>
+    </>
+  )
+}
+
+export default App
