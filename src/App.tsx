ffff2f2eff41bdbdb8a63cf71b379acb28045b83
hebirong <hebirong@igengmei.com>
2024-08-07 16:58:34 +0800

    fix(test): test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+// import "./assets/style/var.less"
+// import WelcomeScreen from "./pages/main/WelcomeScreen";
+// function App() {
+//   return (
+//     <>
+//     <WelcomeScreen />
+//     </>
+//   );
+// }
+// export default App;
+
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/main/WelcomeScreen";
 import "./assets/style/var.less"
-import WelcomeScreen from "./pages/main/WelcomeScreen";
 function App() {
   return (
-    <>
-    <WelcomeScreen />
-    </>
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 export default App;
